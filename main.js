8e284c24483cf37c83efff6ce856b0974fad795f
ANA123BEATRI <131681723+ANA123BEATRI@users.noreply.github.com>
2024-04-12 12:20:33 -0300

    fghgh

--- main.js
+++ main.js
@@ -20,8 +20,9 @@ const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 const tempoObjetivo5 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo6 = new Date("2024-02-01T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4,tempoObjetivo5];
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4,tempoObjetivo5,tempoObjetivo6];
 
 
 function calculaTempo(tempoObjetivo) {
